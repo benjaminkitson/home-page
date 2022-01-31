beaa05b5ceafdf4548b0515b2e260165ceda5cc2
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-31 14:30:53 +0000

    more working on carousel logic

--- js/client.js
+++ js/client.js
@@ -12,13 +12,32 @@ let pv
 function assign() {
   nx = (i === n) ? 0 : i + 1
   pv = (i === 0) ? n : i - 1
+  images[nx].classList.add('nx')
+  images[pv].classList.add('pv')
 }
+
+assign()
 
 next.addEventListener('mouseup', (e) => {
   e.preventDefault();
-  i = (i === n) ? 0 : i + 1
-  assign()
-  console.log(pv, i, nx)
-  // i = (i === 2) ? 0 : i + 1
-  // images[i].classList.add('visible')
+  const center = images[i];
+  const right = images[nx];
+  const left = images[pv];
+
+  // center.addEventListener('transitionend', () => {
+  //   center.classList.remove('visible')
+  // })
+
+  left.classList.add('visible')
+  center.classList.add('move-right')
+  right.classList.remove('nx');
+  left.classList.remove('pv');
+  setTimeout(() => {
+    center.classList.remove('visible');
+    i = (i === n) ? 0 : i + 1;
+    images[i].classList.add('visible')
+    assign();
+  }, 600);
+
+
 })
