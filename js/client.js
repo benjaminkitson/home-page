5175694c6182bb3109b6b853f684537aa1b7e83e
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-31 18:19:33 +0000

    more carousel stuff

--- js/client.js
+++ js/client.js
@@ -1,43 +1,14 @@
 const next = document.querySelector('.next.change');
 const prev = document.querySelector('.prev.change');
-const images = document.querySelectorAll('img');
+const images = Array.from(document.querySelectorAll('img'));
 
 console.log(next)
 
 let n = images.length - 1
-let i = 0
-let nx
-let pv
-
-function assign() {
-  nx = (i === n) ? 0 : i + 1
-  pv = (i === 0) ? n : i - 1
-  images[nx].classList.add('nx')
-  images[pv].classList.add('pv')
-}
-
-assign()
+let i = 1
+let nx = 2
+let pv = 0
 
 next.addEventListener('mouseup', (e) => {
-  e.preventDefault();
-  const center = images[i];
-  const right = images[nx];
-  const left = images[pv];
-
-  // center.addEventListener('transitionend', () => {
-  //   center.classList.remove('visible')
-  // })
-
-  left.classList.add('visible')
-  center.classList.add('move-right')
-  right.classList.remove('nx');
-  left.classList.remove('pv');
-  setTimeout(() => {
-    center.classList.remove('visible');
-    i = (i === n) ? 0 : i + 1;
-    images[i].classList.add('visible')
-    assign();
-  }, 600);
-
 
 })
