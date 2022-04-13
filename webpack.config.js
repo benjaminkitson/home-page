1a4e62797f4a1d77a7a05a31e494aac5188c1c20
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-13 12:08:53 +0100

    added babel and webpack configs, updates scripts

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const { ContextReplacementPlugin } = require('webpack');
+
+function buildPath(relative) {
+  return path.join(__dirname, relative);
+}
+
+module.exports = function(env) {
+
+  const isProduction = (env.production === true);
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: buildPath('public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        test: /\.s?css$/
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      static: {
+        directory: buildPath('public')
+      },
+      historyApiFallback: true
+    }
+  };
+};
