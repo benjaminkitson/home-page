3d81d8d54434d1e8499ebaa7a418dfa949ce7ab5
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-13 13:41:49 +0100

    implemented navigation between about me and home pages

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+
 
 function Navbar() {
+
+  function isActive(navData) {
+    return naveData.isActive ? "current-page" : ""
+  };
+
   return (
     <div className="navbar">
-      navbar
+      <NavLink to='/' className={(navData) => navData.isActive ? "navbar__link current-page" : "navbar__link"}>Home</NavLink>
+      <NavLink to='/about' className={(navData) => navData.isActive ? "navbar__link current-page" : "navbar__link"}>About me</NavLink>
     </div>
   )
 }
